26aa6309bf9474749335fb96a4b1040c583e3012
Vladimir <vladimir.bukvic@live.com>
2021-09-09 09:37:54 +0200

    remove unneeded files

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,8 +1,11 @@
+import App from "../App.svelte";
 import SignUpPage from "./SignUpPage.svelte";
 import { render, screen } from "@testing-library/svelte";
+import '@testing-library/jest-dom'
 
 it("has a Sign  Up header", () => {
-  render(SignUpPage);
+  render(App);
   const header = screen.getByRole("heading", { name: "Sign Up" });
-  expect(header).toBeTruthy();
+  //expect(header).toBeTruthy();
+  expect(header).toBeInTheDocument();
 });
