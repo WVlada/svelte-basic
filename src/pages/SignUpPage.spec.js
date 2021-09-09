3744402aab6709caa3d0572388720613e40c64e6
Vladimir <vladimir.bukvic@live.com>
2021-09-09 14:25:33 +0200

    still basic

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,11 +1,36 @@
 import App from "../App.svelte";
 import SignUpPage from "./SignUpPage.svelte";
 import { render, screen } from "@testing-library/svelte";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 
-it("has a Sign  Up header", () => {
-  render(App);
-  const header = screen.getByRole("heading", { name: "Sign Up" });
-  //expect(header).toBeTruthy();
-  expect(header).toBeInTheDocument();
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has a Sign  Up header", () => {
+      render(SignUpPage);
+      const header = screen.getByRole("heading", { name: "Sign Up" });
+      //expect(header).toBeTruthy();
+      expect(header).toBeInTheDocument();
+    });
+    it("has a username input", () => {
+      render(SignUpPage);
+      //const input = screen.getByPlaceholderText('username')
+      const input = screen.getByLabelText('Username')//returns element associated with label
+      expect(input).toBeInTheDocument();
+    });
+    it("has a email input", () => {
+      //const {container} = render(SignUpPage);
+      //const input = container.querySelector("input");
+      //expect(input).toBeInTheDocument();
+      //expect(container.querySelectorAll('input').length).toBe(2)
+      render(SignUpPage);
+      const input = screen.getByLabelText('Email')//returns element associated with label
+      expect(input).toBeInTheDocument();
+
+    });
+    it("has a password input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText('Password')
+      expect(input).toBeInTheDocument();
+    });
+  });
 });
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,1 +1,7 @@
 <h1>Sign Up</h1>
+<label for="username">Username</label>
+<input id="username" placeholder="username" />
+<label for="email">Email</label>
+<input id="email" placeholder="email"/>
+<label for="password">Password</label>
+<input id="password" placeholder="password"/>
