ecfcfd5c2f873f1566ace06fc9b38b58f9c5688f
Vladimir Bukvić <vladimir.bukvic@live.com>
2021-09-16 22:01:19 +0200

    phase 2

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,6 +1,7 @@
 import App from "../App.svelte";
 import SignUpPage from "./SignUpPage.svelte";
 import { render, screen } from "@testing-library/svelte";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
 describe("Sign Up Page", () => {
@@ -14,7 +15,7 @@ describe("Sign Up Page", () => {
     it("has a username input", () => {
       render(SignUpPage);
       //const input = screen.getByPlaceholderText('username')
-      const input = screen.getByLabelText('Username')//returns element associated with label
+      const input = screen.getByLabelText("Username"); //returns element associated with label
       expect(input).toBeInTheDocument();
     });
     it("has a email input", () => {
@@ -23,14 +24,49 @@ describe("Sign Up Page", () => {
       //expect(input).toBeInTheDocument();
       //expect(container.querySelectorAll('input').length).toBe(2)
       render(SignUpPage);
-      const input = screen.getByLabelText('Email')//returns element associated with label
+      const input = screen.getByLabelText("Email"); //returns element associated with label
       expect(input).toBeInTheDocument();
-
     });
     it("has a password input", () => {
       render(SignUpPage);
-      const input = screen.getByLabelText('Password')
+      const input = screen.getByLabelText("Password");
       expect(input).toBeInTheDocument();
+    });
+    it("has a password type input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password");
+      expect(input.type).toBe("password");
+    });
+    it("has a password repeat input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password repeat");
+      expect(input).toBeInTheDocument();
+    });
+    it("has a password repeat type input", () => {
+      render(SignUpPage);
+      const input = screen.getByLabelText("Password repeat");
+      expect(input.type).toBe("password");
+    });
+    it("has a Sign Up button", () => {
+      render(SignUpPage);
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeInTheDocument();
+    });
+    it("has a Sign Up button is disabled", () => {
+      render(SignUpPage);
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeDisabled();
+    });
+  });
+  describe("Interactions", () => {
+    it("Enables the button when the password and repeat fieeldshave same value", async () => {
+      render(SignUpPage);
+      const passInput = screen.getByLabelText("Password");
+      const passRepeatInput = screen.getByLabelText("Password repeat");
+      await userEvent.type(passInput, "P4ssword");
+      await userEvent.type(passRepeatInput, "P4ssword");
+      const button = screen.getByRole("button", { name: "Sign Up" });
+      expect(button).toBeEnabled();
     });
   });
 });
--- src/pages/SignUpPage.svelte
+++ src/pages/SignUpPage.svelte
@@ -1,7 +1,33 @@
+<script>
+    let disabled = true;
+    let password, passwordRepeat;
+    //const onChangePassword = (event) => {
+    //    password = event.target.value
+    //    //refreshDisabled()
+    //}
+    //const onChangePasswordRepeat = (event) => {
+    //    passwordRepeat = event.target.value
+    //    //refreshDisabled()
+    //}
+    //const refreshDisabled = ()=>{
+    //    disabled = password != passwordRepeat
+    //    //if(password === passwordRepeat){
+    //    //    disabled = false
+    //    //}else{
+    //    //    disabled = true
+    //    //}
+    //}
+    // svelte reactive stuff
+    $: disabled = (password && passwordRepeat) ?  password != passwordRepeat : true
+</script>
+
 <h1>Sign Up</h1>
 <label for="username">Username</label>
 <input id="username" placeholder="username" />
 <label for="email">Email</label>
 <input id="email" placeholder="email"/>
 <label for="password">Password</label>
-<input id="password" placeholder="password"/>
+<input id="password" type="password" placeholder="password" bind:value={password} />
+<label for="password-repeat">Password repeat</label>
+<input id="password-repeat" type="password" placeholder="password" bind:value={passwordRepeat} />
+<button {disabled}>Sign Up</button>
